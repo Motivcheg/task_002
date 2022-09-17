85a6fb75fe6fe79ea893510d0655b59bce8cdafd
Max <lomanuy@gmail.com>
2022-09-17 14:55:28 +0300

    task_25, task_29 complete

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -1,3 +1,16 @@
 ﻿//Задача 25: Напишите цикл, который принимает 
 //на вход два числа (A и B) и возводит число A в натуральную степень B.
 
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int result = 0;
+for (int i = 0; i < numberB; i++)
+{
+    result = numberA * numberA;
+    for (int j = 0; j < i; j++)
+    {
+        result = result * numberA;
+        
+    }
+}
+Console.WriteLine(result);   
--- task_29/Program.cs
+++ task_29/Program.cs
@@ -1,2 +1,25 @@
 ﻿//Задача 29: Напишите программу, которая 
 //задаёт массив из 8 элементов и выводит их на экран.
+
+int[] FillArray(int[] filling)
+{
+for (int i = 0; i < filling.Length; i++)
+{
+filling[i] = new Random().Next(0, 100);
+
+}
+return filling;
+}
+
+void printArray(int[] writing)
+{
+for (int i = 0; i < writing.Length; i++)
+{
+Console.WriteLine(writing[i]);
+}
+}
+
+int[] myArray = new int[8];
+
+FillArray(myArray);
+printArray(myArray);

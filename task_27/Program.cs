5019c73fd7fdf516016a8e5061ddb5c547046e3e
Max <lomanuy@gmail.com>
2022-09-17 13:38:16 +0300

    task_27 complete

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 27: Напишите программу,
+//которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = int.Parse(Console.ReadLine());
+int sum(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(sum(number));
